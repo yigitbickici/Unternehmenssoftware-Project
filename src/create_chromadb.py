a18def868202f7caa7f5ccc509ccfee7bc96eec0
yigitbickici <99952042+yigitbickici@users.noreply.github.com>
2024-05-29 18:28:35 +0200

    .

--- src/create_chromadb.py
+++ src/create_chromadb.py
@@ -2,19 +2,22 @@ import chromadb
 from chromadb.config import Settings
 
 def create_chromadb_collection(name="my_collection"):
-    
     client = chromadb.Client(Settings())
     collection = client.create_collection(name=name)
     return collection
 
 def add_data_to_collection(collection, df):
-   
-    for index, row in df.iterrows():
+    counter = 0
+    for _, row in df.iterrows():
         vector = row.tolist()
-        metadata = row.fillna('').to_dict()  # NaN değerlerini boş bir dizeyle doldur
+        metadata = {}  # Boş bir metadata sözlüğü oluştur
+        # Her bir sütunun adı ve değeri için metadata sözlüğüne ekleyin
+        for column_name, value in row.items():
+            metadata[column_name] = value
         document = {
-            "id": str(index),
+            "id": str(counter),
             "vector": vector,
             "metadata": metadata
         }
-        collection.add([document])  # Koleksiyona belge eklemek için uygun metodu kullanın
+        collection.add([document])
+        counter += 1
