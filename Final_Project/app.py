3691c9aec8120ea5b15ea09e63fe26a1df969979
yigitbickici <99952042+yigitbickici@users.noreply.github.com>
2024-07-06 14:21:46 +0200

    github added

--- Final_Project/app.py
+++ Final_Project/app.py
@@ -1,102 +1,45 @@
-"""
 import streamlit as st
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
-from template import css, bot_template, user_template
-
-def get_cv_content(cv_docs):
-    content = ""
-    for pdf in cv_docs:
-        cv_reader = PdfReader(pdf)
-        for page in cv_reader.pages:
-            content += page.extract_text()
-    return content
-
-
-def get_content_chunks(raw_content):
-    content_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len)
-    chunks = content_splitter.split_text(raw_content)
-    return chunks
-
-
-def get_vstore(chunk_contents):
-    embeddings = OpenAIEmbeddings()
-    v_store = FAISS.from_texts(texts=chunk_contents, embedding=embeddings)
-    return v_store
-
-
-def get_conversation_chain(v_store):
-    llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=v_store.as_retriever(), memory=memory)
-    return chain
-
-
-def handle_input(question):
-    ai_response = st.session_state.conversation({'question': question})
-    st.session_state.chat_history = ai_response['chat_history']
-
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-
-
-def main():
-    load_dotenv()
-    st.set_page_config(page_title="Chat with CVs")
-    st.write(css, unsafe_allow_html=True)
-
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
-
-    st.header("Chat with CVs")
-    question = st.text_input("Ask a question about the personal information from CV:")
-    if question:
-        handle_input(question)
-
-    with st.sidebar:
-        st.subheader("Your CVs")
-        pdf_docs = st.file_uploader("Upload your CV here and click on 'Run the CV(s)'",accept_multiple_files=True)
-        if st.button("Run the CV(s)"):
-            with st.spinner("In progress"):
-                # get pdf text
-                raw_content = get_cv_content(pdf_docs)
-
-                # get the text chunks
-                content_chunks = get_content_chunks(raw_content)
-
-                # create vector store
-                v_store = get_vstore(content_chunks)
-
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(v_store)
-
-
-if __name__ == '__main__':
-    main()
-"""
-
-import streamlit as st
-from dotenv import load_dotenv
+import os
 import pickle
+import re
+from github import Github
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
-import os
+
+
+class GitHubAgent:
+    def __init__(self, token):
+        self.g = Github(token)
+
+    def get_user_repos(self, username):
+        try:
+            user = self.g.get_user(username)
+            repos = user.get_repos()
+            return [repo.name for repo in repos]
+        except Exception as e:
+            return str(e)
+
+    def _extract_repo_name(self, repo_url):
+        match = re.match(r'https://github.com/([^/]+/[^/]+)', repo_url)
+        if match:
+            return match.group(1)
+        else:
+            raise ValueError("Invalid GitHub repository URL")
+
+    def _get_all_contents(self, repo, contents, all_files=[]):
+        for content_file in contents:
+            if content_file.type == "dir":
+                all_files = self._get_all_contents(repo, repo.get_contents(content_file.path), all_files)
+            else:
+                all_files.append(content_file.path)
+        return all_files
+
 
 # Sidebar contents
 with st.sidebar:
@@ -111,16 +54,32 @@ with st.sidebar:
 
 load_dotenv()
 
+
+def extract_github_username(text):
+    # Basic regex to extract GitHub usernames or URLs
+    github_url_pattern = r"https://github\.com/([a-zA-Z0-9-]+)"
+    github_username_pattern = r"\bgithub\.com/([a-zA-Z0-9-]+)\b"
+
+    urls = re.findall(github_url_pattern, text)
+    usernames = re.findall(github_username_pattern, text)
+
+    return urls + usernames
+
+
 def main():
     st.header("Chat with PDF 💬")
 
     # Sistem promptu
     system_prompt = (
         "You are a head hunter assistant who can answer questions based on the content of a CV file."
-        "Your task is giving information about the person I provide you, and help me to whether I hire the person or not."
-        "You should provide clear and concise answers, and if the information is not present in the PDF, "
-        "please indicate that the information is not available."
+        "You need to analyze informations from CV file and can make comments about the owner of the CV"
+        "Your task is giving information about the the owners of the CV files I will provide you, and help me to whether I hire the person or not."
+        "You should provide clear and concise answers because people who are hiring will ask you questions"
     )
+
+    # GitHub Token from environment variables
+    github_token = os.getenv('GITHUB_TOKEN')
+    github_agent = GitHubAgent(github_token)
 
     # Upload PDF files
     pdf_files = st.file_uploader("Upload your PDF(s)", type='pdf', accept_multiple_files=True)
@@ -135,24 +94,29 @@ def main():
             for page in pdf_reader.pages:
                 text += page.extract_text()
 
+            github_usernames = extract_github_username(text)
+            if github_usernames:
+                st.write(f"Found GitHub profiles: {github_usernames}")
+
             text_splitter = RecursiveCharacterTextSplitter(
                 chunk_size=1000,
                 chunk_overlap=200,
                 length_function=len
             )
             chunks = text_splitter.split_text(text=text)
-            
+
             store_name = pdf_file.name[:-4]
             st.write(f'{store_name}')
 
-            if f"{store_name}.pkl" in vector_stores:
-                VectorStore = vector_stores[f"{store_name}.pkl"]
-            else:
-                embeddings = OpenAIEmbeddings()
-                VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-                vector_stores[f"{store_name}.pkl"] = VectorStore
-                with open(f"{store_name}.pkl", "wb") as f:
-                    pickle.dump(VectorStore, f)
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            vector_stores[store_name] = VectorStore
+
+            # Save the FAISS index
+            with open(f"{store_name}_index.pkl", "wb") as f:
+                pickle.dump(VectorStore.index, f)
+            with open(f"{store_name}_texts.pkl", "wb") as f:
+                pickle.dump(chunks, f)
 
         query = st.text_input("Ask questions about your PDF file:")
 
@@ -167,10 +131,15 @@ def main():
                     response = chain.run(input_documents=docs, question=query, system_prompt=system_prompt)
                     responses.append(response)
                     print(cb)
-            
+
             for response in responses:
                 st.write(response)
 
+            if "github" in query.lower():
+                for username in github_usernames:
+                    repos = github_agent.get_user_repos(username)
+                    st.write(f"GitHub Repositories for {username}: {repos}")
+
+
 if __name__ == '__main__':
     main()
-
